3ca86ae9d1e57a8dc445c1b6eeaa6f7a2c02925e
Roberto <roberto.britz@hotmail.com>
2023-10-31 20:13:35 -0300

    HEY-10: creating seeder and factory for question model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    use HasFactory;
     //Adicionado no App Provider
     //protected $guarded = []; // libera a o massa assign de todos os campos.
     //protected $fillable = ['question']; // libera somente o item espessífico
