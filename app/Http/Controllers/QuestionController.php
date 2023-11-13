c19fba116aa50c43cea81602cac54166ce390193
robertobritz <roberto.britz@hotmail.com>
2023-11-13 19:59:32 -0300

    hey-17: it shoul be able to update a question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -48,6 +48,15 @@ class QuestionController extends Controller
         return view('question.edit', compact('question'));
     }
 
+    public function update(QUestion $question): RedirectResponse
+    {
+        $question->question = request()->question;
+
+        $question->save();
+
+        return back();
+    }
+
     public function destroy(Question $question): RedirectResponse
     {
         $this->authorize('destroy', $question);
